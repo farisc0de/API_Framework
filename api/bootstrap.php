8a5469ac9e8d99054885f6bd0ff6b4077e29d928
Faris AL-Otaibi <76238196+farisc0de@users.noreply.github.com>
2022-01-12 03:27:19 +0300

    Update bootstrap.php

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -7,6 +7,22 @@ header("Content-type: application/json; charset=UTF-8");
 set_error_handler("MY_Framework\ErrorHandler::handleError");
 set_exception_handler("MY_Framework\ErrorHandler::handleException");
 
+use MY_Framework\JWTCodec;
+
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 
 $dotenv->load();
+
+$path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$route = $parts[2];
+
+$id = $parts[3] ?? null;
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+$secret_key = $_ENV['SECRET_KEY'];
+
+$codec = new JWTCodec($secret_key);
